0c41c7442d322b38d505c3e4c382b5c2ddf4ffd9
tobisamuel <ufomatobi@gmail.com>
2022-06-01 12:53:58 +0100

    Removed models.py

--- app.py
+++ app.py
@@ -12,17 +12,74 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
-from models import app, Venue, Artist, Show
 
 # ----------------------------------------------------------------------------#
 # App Config.
 # ----------------------------------------------------------------------------#
 
+app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object("config")
+
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+# ----------------------------------------------------------------------------#
+# Models.
+# ----------------------------------------------------------------------------#
+
+
+class Show(db.Model):
+    __tablename__ = "shows"
+
+    id = db.Column(db.Integer, primary_key=True)
+    venue_id = db.Column(db.ForeignKey("venues.id"))
+    artist_id = db.Column(db.ForeignKey("artists.id"))
+    start_time = db.Column(db.DateTime(), nullable=False)
+
+
+class Venue(db.Model):
+    __tablename__ = "venues"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    address = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500))
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(120))
+    shows = db.relationship(
+        "Show",
+        backref="venue",
+        cascade="all, delete-orphan",
+    )
+
+
+class Artist(db.Model):
+    __tablename__ = "artists"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    city = db.Column(db.String(120), nullable=False)
+    state = db.Column(db.String(120), nullable=False)
+    phone = db.Column(db.String(120), nullable=False)
+    image_link = db.Column(db.String(500))
+    genres = db.Column(db.ARRAY(db.String), nullable=False)
+    facebook_link = db.Column(db.String(120))
+    website = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, nullable=False, default=False)
+    seeking_description = db.Column(db.String(120))
+    shows = db.relationship(
+        "Show",
+        backref="artist",
+        cascade="all, delete-orphan",
+    )
 
 
 # ----------------------------------------------------------------------------#
